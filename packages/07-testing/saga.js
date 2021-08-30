c41bee03ac3b896396eb8e631655dbd6fb242138
wayneliang(梁宇浩) <wayneliang@futunn.com>
2021-08-30 19:48:22 +0800

    feat(完成两个案例的测试):

--- packages/07-testing/saga.js
+++ packages/07-testing/saga.js
@@ -7,18 +7,33 @@ import {
   take,
   fork,
   all,
+  cancel,
 } from "redux-saga/effects";
 
-export function* setColorWhenModeChange(){
-  const action = yield take('CHANGE_MODE')
+export function* setColorWhenModeChange() {
+  const action = yield take("CHANGE_MODE");
   switch (action.payload.mode) {
     case 0:
-      yield put({type:'SET_COLOR',payload:{color:'white'}})
+      yield put({ type: "SET_COLOR", payload: { color: "white" } });
       break;
     case 1:
-      yield put({type:'SET_COLOR',payload:{color:'black'}})
+      yield put({ type: "SET_COLOR", payload: { color: "black" } });
       break;
     default:
       break;
+  }
+}
+
+export function* syncSaga() {}
+
+export function* main() {
+  // 当开关组件被开启，action:({type:'START_SYNC'})被派发，`main`开始进入while语句块执行
+  while (yield take("START_SYNC")) {
+    // 调度syncSaga生成syncTask
+    const syncTask = yield fork(syncSaga);
+    // 等待action:({type:'STOP_SYNC'})被派发
+    yield take("STOP_SYNC");
+    // 当开关组件被关闭后，取消syncTask的执行
+    yield cancel(syncTask);
   }
 }
--- packages/07-testing/saga.test.js
+++ packages/07-testing/saga.test.js
@@ -1,4 +1,4 @@
-import {setColorWhenModeChange} from './saga'
+import { setColorWhenModeChange, main, syncSaga } from "./saga";
 import {
   call,
   put,
@@ -8,25 +8,47 @@ import {
   take,
   fork,
   all,
+  cancel,
 } from "redux-saga/effects";
-import { cloneableGenerator } from '@redux-saga/testing-utils';
+import { cloneableGenerator, createMockTask } from "@redux-saga/testing-utils";
 
-describe('setColorWhenModeChange testing',()=>{
-  const gen = cloneableGenerator(setColorWhenModeChange)()
+describe("setColorWhenModeChange testing", () => {
+  const gen = cloneableGenerator(setColorWhenModeChange)();
 
-  test("test: yield take('CHANGE_MODE')",()=>{
-    expect(gen.next().value).toEqual(take('CHANGE_MODE'))
-  })
+  test("test: yield take('CHANGE_MODE')", () => {
+    expect(gen.next().value).toEqual(take("CHANGE_MODE"));
+  });
 
-  test("test: yield put({type:'SET_COLOR',payload:{color:'white'}})",()=>{
-    const clone = gen.clone()
-    expect(clone.next({type:'CHANGE_MODE',payload:{mode:0}}).value)
-      .toEqual(put({type:'SET_COLOR',payload:{color:'white'}}))
-  })
+  test("test: yield put({type:'SET_COLOR',payload:{color:'white'}})", () => {
+    const clone = gen.clone();
+    expect(
+      clone.next({ type: "CHANGE_MODE", payload: { mode: 0 } }).value
+    ).toEqual(put({ type: "SET_COLOR", payload: { color: "white" } }));
+  });
 
-  test("test: yield put({type:'SET_COLOR',payload:{color:'white'}})",()=>{
-    const clone = gen.clone()
-    expect(clone.next({type:'CHANGE_MODE',payload:{mode:1}}).value)
-      .toEqual(put({type:'SET_COLOR',payload:{color:'black'}}))
-  })
-})
+  test("test: yield put({type:'SET_COLOR',payload:{color:'white'}})", () => {
+    const clone = gen.clone();
+    expect(
+      clone.next({ type: "CHANGE_MODE", payload: { mode: 1 } }).value
+    ).toEqual(put({ type: "SET_COLOR", payload: { color: "black" } }));
+  });
+});
+
+describe("main testing", () => {
+  const gen = main();
+
+  test('test: yield take("START_SYNC")', () => {
+    expect(gen.next().value).toEqual(take("START_SYNC"));
+  });
+
+  test("test: yield fork(syncSaga)", () => {
+    const mockAction = { type: "START_SYNC" };
+    expect(gen.next(mockAction).value).toEqual(fork(syncSaga));
+  });
+
+  test('test: yield take("STOP_SYNC") and yield cancel(syncTask)', () => {
+    const mockTask = createMockTask();
+    expect(gen.next(mockTask).value).toEqual(take("STOP_SYNC"));
+    expect(gen.next().value).toEqual(cancel(mockTask));
+  });
+});
